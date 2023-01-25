afeb8301232bb863831498b9dee4e3b3f27737ae
sgavin0813 <s.gavin0813@gmail.com>
2023-01-25 17:29:22 -0500

    when logic corrected

--- app/src/main/java/com/example/bmi_gavin/MainActivity.kt
+++ app/src/main/java/com/example/bmi_gavin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bmi_gavin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import android.widget.TextView
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //used to connect kotlin/java to xml file
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //setContentView is where we actually start coding part
-        
+
         val textView:TextView = findViewById(R.id.resultText)
         //findViewById() is a method ->  used to retrieve a view in an Activity by its unique identifier
         //R.id is subclass of 'R' and R.id.weight is reference to a view with id weight.
@@ -30,27 +32,22 @@ class MainActivity : AppCompatActivity() {
 
 class MyOnClickListener(private val editWeight:EditText,private val editHeightF:EditText,private val editHeightI:EditText,val textView:TextView) : View.OnClickListener {
     override fun onClick(v: View?) {
-        val weight = (editWeight.text.toString()).toInt()
-        val heightI = (editHeightI.text.toString()).toInt()
-        val heightF2I = (editHeightF.text.toString()).toInt() * 12
-        val heightInches  = heightF2I * heightI
+        val weight = (editWeight.text.toString()).toDouble()
+        val heightI = (editHeightI.text.toString()).toDouble()
+        val heightF = (editHeightF.text.toString()).toDouble()
+        val heightInches  = (heightF * 12) +  heightI
         val bmi = (weight / (heightInches * heightInches)) * 703
-        when {
-            bmi < 18.5 -> {
-                textView.text = "Underweight"
-            }
-            bmi < 25 -> {
-                textView.text = "Normal weight"
-            }
-            bmi < 30 -> {
-                textView.text = "Overweight"
-            }
-            else -> {
-                textView.text = "Obese"
-            }
-        }
 
-    }
+            when {
+                bmi < 18.5 -> textView.text = "Skinny Bitch"
+                bmi in 18.5..24.9 -> textView.text = "Good Job"
+                bmi in 25.0..29.9 -> textView.text = "Fat ass"
+                else -> textView.text = "Get a Gym Membership"
+            }
+
+
+
+        }
 }
 
 
